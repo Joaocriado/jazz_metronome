ecb323694dd390490bd7c8a1cbda7070c2fc5617
Joaocriado <Joaogenie90@hotmail.fr>
2023-05-25 09:44:17 +0200

    Update 230214_metronome.py

--- 230214_metronome.py
+++ 230214_metronome.py
@@ -15,8 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License along 
 # with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# Icons used are from https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
 
 import os
 import readline #history for input with arrows
